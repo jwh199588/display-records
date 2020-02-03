c4c6f8fdbd53f094d1d618d9e11577a09243dcc3
jwh199588 <1262466460@qq.com>
2020-02-03 10:28:41 +0800

    Create Constant.java

---
+++ src/main/com/cn/jwh/Constant.java
@@ -0,0 +1,13 @@
+package main.com.cn.jwh;
+
+/**
+ * @ClassName Constant
+ * @Auther: JIAWENHAO
+ * @Date: 2020/2/2 17:02
+ * @Description: TODO
+ */
+
+public class Constant {
+
+    public static String filePath = "D:\\project\\plugin\\text\\";
+}
